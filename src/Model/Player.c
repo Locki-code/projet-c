7f17726cf17af3bb1cadf3265da6a40d77fdb066
Jacques MUDOY <jacques@mudoy.fr>
2023-05-10 11:41:17 +0200

    Create player and enemies (struct and constructor)

---
+++ src/Model/Player.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//Joueur principal
+struct  player_
+{
+    int pv_max;
+    int pv;
+    int attack;
+    int defense;
+    int nb_cle;
+
+};
+typedef struct player_ * Player;
+
+Player CreatePlayer()
+{
+    Player nouv = malloc(sizeof(struct player_));
+    nouv->pv_max = 10;
+    nouv->pv = 10;
+    nouv->attack = 2;
+    nouv->defense = 1;
+    nouv->nb_cle = 0;
+}
