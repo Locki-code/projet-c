b56d1253b7999324114435f974e427a7eba1d574
Loick Mercier <64089744+Locki-code@users.noreply.github.com>
2023-05-17 09:39:20 +0200

    Feature : Added level reader files and level model files (#5)
    Co-authored-by: lmercier <loick_mercier@ens.univ-artois.fr>

--- src/Model/Header/Effect.h
+++ src/Model/Header/Effect.h
@@ -1,3 +1,6 @@
+#ifndef PROJET_C_EFFECT_H
+#define PROJET_C_EFFECT_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -12,4 +15,6 @@ struct effect_ {
 typedef struct effect_ * Effect;
 
 Effect createEffect(char name, bool health, bool attack, bool defense);
+
+#endif //PROJET_C_EFFECT_H
 
