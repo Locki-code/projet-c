b58af23deeb2369b3c72631abc9217679fa11403
Hugo Assoignon <hugoass2002@gmail.com>
2023-05-17 10:12:38 +0200

    création première version menu pour le lancement

---
+++ src/Interface/Menu.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void affichemenu()
+{
+    int choix;
+    printf("1 - Nouvelle partie\n2 - Crédits\n3 - Quitter\n");
+    scanf("%d",&choix);
+    switch(choix)
+    {
+    case 1:
+        /* appellera la fonction d'interface de la nouvelle partie, pour tester ; print nouvelle partie */
+        printf("Nouvelle partie");
+        break;
+    case 2:
+        printf("Voici les crédits :\nHugo Assoignon | Louise Hortier | Jacques Mudoy | Loïck Mercier ");
+        printf("Dernière modif : 10/5/23");
+        break;
+    case 3:
+        printf("Vous avez mis fin au jeu !!");//exit qui termine le porgramme avec le code succès qui correspond à la valeur 0 lors du return
+        exit(EXIT_SUCCESS);
+        break;
+    default:
+    printf("erreur");
+        break;
+    }
+}
+
+int main()
+{
+    affichemenu();
+    return 0;
+}
