e6b977aeb60eb78f2f98a4d29ceec82cde385d3c
Loick Mercier <64089744+Locki-code@users.noreply.github.com>
2023-06-08 13:39:23 +0200

    Added read gate level (#17)
    ---------
    
    Co-authored-by: lmercier <loick_mercier@ens.univ-artois.fr>

--- main.c
+++ main.c
@@ -12,8 +12,9 @@
 
 int main(int argc, char *argv[]) {
     //printf("%s", "ici 1");
-    //Level level = readLevelFile("C:\\Users\\MrTerminalpro\\Documents\\ISEN\\projet-c\\config\\level1.level");
+    Level level = readLevelFile("C:\\Users\\MrTerminalpro\\Documents\\ISEN\\projet-c\\config\\level1.level");
     //wprintf("%lc", getElement(level->matrix, 0, 1));
     //Matrix matrix = newMatrix(5, "##^###   #<   >#   ###v##");
-    //printMatrix(level->matrix);
+    printMatrix(level->matrix);
+    //printf("%c", getElement(level->matrix, 2, 2));
 }
